528321bad94460fb408e2618d3dd755a1569d3b8
Nate <NateThompson114@gmail.com>
2023-02-04 12:32:22 -0500

    Added another decency that requires another to be registered.

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<Demo>();
+builder.Services.AddTransient<ProcessDemo>();
 
 var app = builder.Build();
 
