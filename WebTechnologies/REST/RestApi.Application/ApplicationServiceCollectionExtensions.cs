2512e94fb9bdb8712abcb095dc391b1d44e0fcf5
Nate Thompson <nthompson2@avidxchange.com>
2023-02-20 21:28:07 -0500

    Added repository and added service collection to RestApi.Application.csproj to ensure encapsulation of concerns.

---
+++ WebTechnologies/REST/RestApi.Application/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RestApi.Application.Repositories;
+
+namespace RestApi.Application;
+
+public static class ApplicationServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddSingleton<IMovieRepository, MovieRepository>();
+        return services;
+    }
+}
--- WebTechnologies/REST/RestApi/Program.cs
+++ WebTechnologies/REST/RestApi/Program.cs
@@ -1,6 +1,22 @@
+using RestApi.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+// builder.Services.AddSingleton<IMovieRepository, MovieRepository>(); // This is the normal method, does not encapsulates properly
+
 app.MapGet("/", () => "Hello World!");
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
 
 app.Run();
