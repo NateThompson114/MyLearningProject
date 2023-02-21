baba2aee73b3aba4c32703d1109c48f0cf709b61
Nate Thompson <nthompson2@avidxchange.com>
2023-02-20 22:08:20 -0500

    Removed the magic strings to have a more predictable and synchronized version of routing

--- WebTechnologies/REST/RestApi/Controllers/MoviesController.cs
+++ WebTechnologies/REST/RestApi/Controllers/MoviesController.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using RestApi.Application.Models;
 using RestApi.Application.Repositories;
 using RestApi.Contracts.Request;
-using RestApi.Contracts.Responses;
 using RestApi.Mapping;
 
 namespace RestApi.Controllers;
 
 [ApiController]
-[Route("api")]
 public class MoviesController : Controller
 {
     private readonly IMovieRepository _movieRepository;
@@ -18,7 +15,7 @@ public class MoviesController : Controller
         _movieRepository = movieRepository;
     }
 
-    [HttpPost("movies")]
+    [HttpPost(ApiEndPoints.Movies.Create)]
     public async Task<IActionResult> Create([FromBody]CreateMovieRequest request)
     {
         var movie = request.MapToMovie();
@@ -27,6 +24,6 @@ public class MoviesController : Controller
 
         var movieResponse = movie.MapToMovieResponse();
 
-        return Created($"/api/movies/{movie.Id}", movieResponse);
+        return Created($"/{ApiEndPoints.Movies.Create}/{movie.Id}", movieResponse);
     }
 }
