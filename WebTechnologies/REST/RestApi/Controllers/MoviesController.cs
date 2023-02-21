0c1a2f75a608cee1b34c50c9a1e766f5c437f604
Nate Thompson <nthompson2@avidxchange.com>
2023-02-20 21:31:12 -0500

    Added Controller and methods

---
+++ WebTechnologies/REST/RestApi/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestApi.Application.Repositories;
+using RestApi.Contracts.Request;
+
+namespace RestApi.Controllers;
+
+[ApiController]
+[Route("api")]
+public class MoviesController : Controller
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public MoviesController(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+
+    [HttpPost("movies")]
+    public async Task<IActionResult> Create([FromBody]CreateMovieRequest request)
+    {
+        return Ok(request);
+    }
+}
