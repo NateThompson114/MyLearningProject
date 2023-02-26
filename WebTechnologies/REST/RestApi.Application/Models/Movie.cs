4b510b4c08308502e097693ef87b185a20633815
Nate Thompson <nthompson2@avidxchange.com>
2023-02-26 10:45:31 -0500

    Added slug and way to use the get response with more than one option.

--- WebTechnologies/REST/RestApi.Application/Models/Movie.cs
+++ WebTechnologies/REST/RestApi.Application/Models/Movie.cs
@@ -1,9 +1,23 @@
-﻿namespace RestApi.Application.Models;
+﻿using System.Text.RegularExpressions;
 
-public class Movie
+namespace RestApi.Application.Models;
+
+public partial class Movie
 {
     public required Guid Id { get; init; }
     public required string Title { get; set; }
+    public string Slug => GenerateSlug();
+
+    private string GenerateSlug()
+    {
+        var slugTitle = SlugRegex().Replace(Title, string.Empty)
+            .ToLower().Replace(" ", "-");
+        return $"{slugTitle}-{YearOfRelease}";
+    }
+
     public required int YearOfRelease { get; set; }
     public required List<string> Genres { get; init; } = new();
+
+    [GeneratedRegex("[^0-9A-Za-z _-]", RegexOptions.NonBacktracking, 5)]
+    private static partial Regex SlugRegex();
 }
