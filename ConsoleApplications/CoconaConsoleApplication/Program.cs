89fa128b14cc902e0471eef3d286df6bc5f32680
Nathan Thompson <nthompson2@avidxchange.com>
2023-12-28 13:07:32 -0500

    Add CoconaConsoleApplication project and related files, including Program.cs, WeatherCommands.cs, and IWeatherService.cs.

---
+++ ConsoleApplications/CoconaConsoleApplication/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using Cocona;
+using CoconaConsoleApplication;
+using CoconaConsoleApplication.Weather;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+// // Run Name --lastname LastName
+// CoconaApp.Run(([Argument] string name, [Option] string? lastname) =>
+// {
+//     Console.WriteLine($"Hello, {name} {lastname}!");    
+// });
+
+var builder = CoconaApp.CreateBuilder();
+
+builder.Services.AddLogging(logging =>
+{
+    logging.AddFilter("System.Net.Http");
+});
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<IWeatherService, OpenWeatherMapService>();
+
+var app = builder.Build();
+
+// app.AddCommand("weather", async (IWeatherService weatherService) =>
+// {
+//     var weather = await weatherService.GetWeatherForCityAsync("London");
+//     Console.WriteLine(JsonSerializer.Serialize(weather, new JsonSerializerOptions{WriteIndented = true}));
+// });
+
+app.AddCommands<WeatherCommands>();
+
+app.Run();
---
+++ ConsoleApplications/CoconaConsoleApplication/WeatherCommands.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using Cocona;
+using CoconaConsoleApplication.Weather;
+
+namespace CoconaConsoleApplication;
+
+public class WeatherCommands
+{
+    private readonly IWeatherService _weatherService;
+
+    public WeatherCommands(IWeatherService weatherService)
+    {
+        _weatherService = weatherService;
+    }
+    
+    // If you dont add the attribute and only have one command any run command will be accepted
+    // If you have more than one command without the attribute then the name of the method will be used
+    [Command("weather")]
+    public async Task Weather()
+    {
+        var weather = await _weatherService.GetWeatherForCityAsync("London");
+        Console.WriteLine(JsonSerializer.Serialize(weather, new JsonSerializerOptions{WriteIndented = true}));
+    }
+    
+    [Ignore]
+    public async Task Weather2()
+    {
+        var weather = await _weatherService.GetWeatherForCityAsync("London");
+        Console.WriteLine(JsonSerializer.Serialize(weather, new JsonSerializerOptions{WriteIndented = true}));
+    }
+}
