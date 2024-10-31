783f0e1eca6d2083789b2bd2c19e3fee82e7ba28
Nathan Thompson <nthompson2@avidxchange.com>
2024-10-31 15:13:55 -0400

    Add Generic Host and logging configuration
    Integrate Microsoft.Extensions.Hosting for DI and logging setup. Configure JSON console logging and replace direct Console.WriteLine with logger.

--- Logging/GenericHostAndExplicitLogging/Program.cs
+++ Logging/GenericHostAndExplicitLogging/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+//! This is great for services or long-running headless applications.
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging(x =>
+    {
+        x.AddJsonConsole();
+    })
+    .Build();
+
+//! This is how to get access to the logger.
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation("Hello, world!");
+
+host.Run();
