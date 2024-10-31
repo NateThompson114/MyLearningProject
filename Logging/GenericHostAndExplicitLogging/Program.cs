3e1219a4bbae865ebe57aa3e59d8a963d0196c0e
Nathan Thompson <nthompson2@avidxchange.com>
2024-10-31 15:17:16 -0400

    Update logging implementation to use LoggerFactory
    Refactored the code to utilize `LoggerFactory` instead of `IHost` for logger creation. This change simplifies the logger setup and makes the code more modular for custom logging scenarios.

--- Logging/GenericHostAndExplicitLogging/Program.cs
+++ Logging/GenericHostAndExplicitLogging/Program.cs
@@ -1,20 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-//! This is great for services or long-running headless applications.
-using IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureLogging(x =>
-    {
-        x.AddJsonConsole();
-    })
-    .Build();
+// //! This is great for services or long-running headless applications.
+// using IHost host = Host.CreateDefaultBuilder(args)
+//     .ConfigureLogging(x =>
+//     {
+//         x.AddJsonConsole();
+//     })
+//     .Build();
+//
+// //! This is how to get access to the logger.
+// var logger = host.Services.GetRequiredService<ILogger<Program>>();
+//
+// logger.LogInformation("Hello, world!");
+//
+// host.Run();
 
-//! This is how to get access to the logger.
-var logger = host.Services.GetRequiredService<ILogger<Program>>();
+//! The logger factory is used to create loggers. Knowing this, you can create a custom logger, and handle logging in 99% of the cases.
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddJsonConsole();
+});
+
+ILogger logger = loggerFactory.CreateLogger<Program>();
 
 logger.LogInformation("Hello, world!");
-
-host.Run();
