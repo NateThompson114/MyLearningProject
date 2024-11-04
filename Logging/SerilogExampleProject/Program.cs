11c38fe655e5070a36c3479a64ae077079ea1cad
Nathan Thompson <nthompson2@avidxchange.com>
2024-11-04 14:32:33 -0500

    Add file sink to logging configuration
    This commit introduces a file sink to the Serilog configuration, enabling log output to both console and a daily rolling log file. The corresponding NuGet package for Serilog.Sinks.File is also added to the project file.

--- Logging/SerilogExampleProject/Program.cs
+++ Logging/SerilogExampleProject/Program.cs
@@ -6,6 +6,7 @@ using Serilog.Sinks.SystemConsole.Themes;
 //This would produce nothing because serilog while configured, does not have a sink, which is the same as a provider in the Microsoft.Extensions.Logging world.
 var logger = new LoggerConfiguration()
     .WriteTo.Console(theme:AnsiConsoleTheme.Code)
+    .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day, rollOnFileSizeLimit:true )
     .CreateLogger();
 
 logger.Information("Hello, world!"); 
