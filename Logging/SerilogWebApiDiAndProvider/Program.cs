657c4f684bf4a989c585863004e07184a45c6ad6
Nathan Thompson <nthompson2@avidxchange.com>
2024-11-04 14:53:28 -0500

    Add Serilog for logging and configure application pools
    Integrated Serilog for enhanced logging in the web application by including the Serilog.AspNetCore package and configuring the logger in Program.cs. Also, updated applicationhost.config to define new application pools and sites for improved hosting setup.

--- Logging/SerilogWebApiDiAndProvider/Program.cs
+++ Logging/SerilogWebApiDiAndProvider/Program.cs
@@ -1,6 +1,18 @@
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Using Serilog.AspNetCore allows us to use the UseSerilog() extension method on the HostBuilder.
+// This will allow us to use Serilog as the logging provider for the application and hijack the default logging.
+// However, Serilog will not listen to the appsettings.json file, so we need to configure it manually otherwise we lose the ability to hotswap configures for the logger.
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console(theme:AnsiConsoleTheme.Code)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
