d8a022ae70f2a15688ebafb3ca8346bb312771f8
Nathan Thompson <nthompson2@avidxchange.com>
2024-11-04 14:26:53 -0500

    Rename and simplify Serilog demo project
    Replaced 'SerilogDemoProject' with 'SerilogExampleProject' and simplified the project structure. Removed unnecessary JSON configuration files and streamlined the main program to focus on basic Serilog functionality.

---
+++ Logging/SerilogExampleProject/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Serilog;
+
+//This would produce nothing because serilog while configured, does not have a sink, which is the same as a provider in the Microsoft.Extensions.Logging world.
+var logger = new LoggerConfiguration()
+    .CreateLogger();
+
+logger.Information("Hello, world!"); 
