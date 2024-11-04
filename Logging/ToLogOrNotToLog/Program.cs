b09bf87924a8999bf9d790b154911464281e816d
Nathan Thompson <nthompson2@avidxchange.com>
2024-11-04 10:20:02 -0500

    Update logging configuration based on environment
    Refactored the logging setup to clear existing providers and conditionally add console logging for development and Application Insights for other environments. This ensures the logging behavior is customized based on the environment, improving flexibility and clarity.

--- Logging/ToLogOrNotToLog/Program.cs
+++ Logging/ToLogOrNotToLog/Program.cs
@@ -9,11 +9,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Application Insights is a asynchronous logger, so it will not block the main thread, however that also means it takes time for logs to show up in application insights.
-builder.Logging.AddApplicationInsights(
-    configureTelemetryConfiguration: teleConfig =>
-        teleConfig.ConnectionString = "InstrumentationKey=331b798e-ba02-4c30-82c6-6bb30fe33afe;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/;ApplicationId=e1360525-7aef-4c2e-8df6-6f5ed506c2b0",
-    configureApplicationInsightsLoggerOptions: _ => { });
+// This allows you to choose your providers, your customizations to your logging based on anything, in this case the environment.
+builder.Logging.ClearProviders();
+
+if(builder.Environment.IsDevelopment())
+{
+    builder.Logging.AddConsole();
+}
+else
+{
+    // Application Insights is a asynchronous logger, so it will not block the main thread, however that also means it takes time for logs to show up in application insights.
+    builder.Logging.AddApplicationInsights(
+        configureTelemetryConfiguration: teleConfig =>
+            teleConfig.ConnectionString = "InstrumentationKey=331b798e-ba02-4c30-82c6-6bb30fe33afe;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/;ApplicationId=e1360525-7aef-4c2e-8df6-6f5ed506c2b0",
+        configureApplicationInsightsLoggerOptions: _ => { });
+}
+
 
 // builder.Logging.AddFilter<ApplicationInsightsLogger>(filter => { });
 
@@ -31,6 +42,8 @@ builder.Logging.AddApplicationInsights(
 
 var app = builder.Build();
 
+#region Logging Information
+
 // ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
 // logger.LogInformation("Hello, world!");
 // Returns
@@ -45,6 +58,8 @@ var app = builder.Build();
 //
 // LogLevel logLevel = LogLevel.Information; // <-- Uncomment to look at the log level enum
 // LoggerExtensions.LogTrace(logger, "This is a trace log message."); // <-- Uncomment to look at the log extension methods
+
+#endregion
 
 ILogger<Program> logger = app.Services.GetRequiredService<ILogger<Program>>();
 logger
