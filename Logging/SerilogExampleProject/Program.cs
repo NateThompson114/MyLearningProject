e5e1a0939842caeb42c535a05c56379e96baff60
Nathan Thompson <nthompson2@avidxchange.com>
2024-11-04 14:29:59 -0500

    Add console sink to Serilog configuration
    Integrated the console sink with an ANSI theme to the Serilog logger in Program.cs for improved logging output clarity. Updated `SerilogExampleProject.csproj` to include the `Serilog.Sinks.Console` package dependency.

--- Logging/SerilogExampleProject/Program.cs
+++ Logging/SerilogExampleProject/Program.cs
@@ -1,9 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
 
 //This would produce nothing because serilog while configured, does not have a sink, which is the same as a provider in the Microsoft.Extensions.Logging world.
 var logger = new LoggerConfiguration()
+    .WriteTo.Console(theme:AnsiConsoleTheme.Code)
     .CreateLogger();
 
 logger.Information("Hello, world!"); 
