766ee82afa0bf944900a1185fe22080d3b6a79ab
Nathan Thompson <nthompson2@avidxchange.com>
2024-11-05 08:56:00 -0500

    Add HighPerformanceLogging project with PaymentService
    Introduced a new project named HighPerformanceLogging to the solution, including necessary configurations in the solution file. Implemented a PaymentService class with logging capabilities, and set up dependency injection for the service in Program.cs.

---
+++ Logging/HighProfmanceLogging/PaymentService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace HighProfmanceLogging;
+public class PaymentService
+{
+    private readonly ILogger<PaymentService> _logger;
+
+    public PaymentService(ILogger<PaymentService> logger)
+    {
+        _logger = logger;
+    }
+
+    public void CreatePayment(string email, decimal amount, int productId)
+    {
+        // Do some work
+        _logger.LogInformation("Customer {email} purchased product {productId} for {amount}", email, productId, amount);
+    }
+}
---
+++ Logging/HighProfmanceLogging/Program.cs
@@ -0,0 +1,20 @@
+﻿using HighProfmanceLogging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging(logging =>
+    {
+        logging.ClearProviders();
+        logging.AddConsole();
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<PaymentService>();
+    })
+    .Build();
+
+var example = host.Services.GetRequiredService<PaymentService>();
+
+example.CreatePayment("user@email.com", 100.00m, 1);
