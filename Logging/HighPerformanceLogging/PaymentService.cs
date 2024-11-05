7871ecf77914f53eae992b4ca4abf886a1d5f3f3
Nathan Thompson <nthompson2@avidxchange.com>
2024-11-05 09:21:12 -0500

    Optimize PaymentService logging for high performance
    Added better comments.

--- Logging/HighPerformanceLogging/PaymentService.cs
+++ Logging/HighPerformanceLogging/PaymentService.cs
@@ -8,6 +8,9 @@ public class PaymentService
     // This static object will be initialized once and reused for all calls to LogPayment
     // The string, decimal, and int parameters are now generic type parameters and not passed as an array of objects
     // Because they are now generic type parameters, the compiler can inline the calls to LogPayment, and not box the parameters
+    // The event id is a struct so no memory allocation
+    // The format string is a literal, so there is no real overhead
+    // Finally the LogLevel is a enum or value type
     private static readonly Action<ILogger, string, decimal, int, Exception?> LogPayment =
         LoggerMessage.Define<string, decimal, int>(
             LogLevel.Information,
