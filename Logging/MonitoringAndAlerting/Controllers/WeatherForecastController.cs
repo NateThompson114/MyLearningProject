d11949cd4c9fe8f406c5a508ee6ba67b2966ab30
Nathan Thompson <nthompson2@avidxchange.com>
2024-11-07 15:54:52 -0500

    Integrate Serilog for enhanced logging
    Added Serilog configuration, packages, and integration into the .NET application. Implemented timed logging functionality and updated the WeatherForecast controller to utilize it. Modified appsettings to include Serilog settings and added a new app pool in applicationhost.config.

--- Logging/MonitoringAndAlerting/Controllers/WeatherForecastController.cs
+++ Logging/MonitoringAndAlerting/Controllers/WeatherForecastController.cs
@@ -19,14 +19,24 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public async Task<IEnumerable<WeatherForecast>> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        using (_logger.BeginTimedOperation("Weather forecast request"))
+        {
+            await Task.Delay(Random.Shared.Next(10, 100));
+
+            if (Random.Shared.Next(1, 10) > 8)
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                throw new Exception("Oops, something went wrong");
+            }
+            
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
     }
 }
